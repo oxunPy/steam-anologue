5ccc473f05203c49fbe89b2f0504c1bf5ef2afab
acer <csharpoxun@gmail.com>
2025-02-21 14:03:23 +0500

    initial commit

---
+++ src/main/java/org/example/steamanalogue/SteamAnalogueApplication.java
@@ -0,0 +1,13 @@
+package org.example.steamanalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamAnalogueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamAnalogueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/steamanalogue/SteamAnalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.steamanalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamAnalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
